e1201796b713e9c53c0804c3bd78655dd7420f3b
O-xix <99446316+O-xix@users.noreply.github.com>
2022-05-17 14:44:21 -0700

    "Flee" command added.

--- Main.java
+++ Main.java
@@ -1,4 +1,3 @@
-
 import java.awt.*;
 import java.io.*;
 import java.lang.*;
@@ -21,25 +20,33 @@ public class Main {
     }
     public static void battle(Hilda hilda, Enemy enemy) {
         Scanner battle_input = new Scanner(System.in);
-        Random battle_hit_limit = new Random();
-        System.out.println("The " + enemy.name_type + " nearly stepped on you!");
-        while (enemy.enemyHP > 0) {
+        Random battle_random = new Random();
+        System.out.println("The " + enemy.name_type + " nearly stepped on you! \n");
+        boolean fleed = false;
+        while (enemy.enemyHP > 0 && fleed == false) {
             //Player attack
             System.out.println("What will you do?");
             System.out.println("    FIGHT (only working one)   MAGIC");
             System.out.println("    ITEMS   FLEE \n\n");
-            
+
             String battle_command = battle_input.nextLine();
-            if (battle_command == "FIGHT") {
-                System.out.print("Hello!");
-                hilda.hildaAttack(enemy, battle_hit_limit);
+            System.out.println("\n\n");
+            if(battle_command.equals("FIGHT")) {
+                hilda.hildaAttack(enemy, battle_random);
+            }
+            else if(battle_command.equals("FLEE")) {
+                fleed = hilda.hildaFlee(battle_random);
+                if (fleed = true) {
+                    break;FLEE
+                }
             }
             else {
+                System.out.println("Error.");
                 continue;
             }
-            
+
             //Enemy attack
-            enemy.enemyAttack(hilda, battle_hit_limit);
+            enemy.enemyAttack(hilda, battle_random);
         }
     }
 }
