931d6b0d91a52ef821607c5798d2db19968a6851
O-xix <99446316+O-xix@users.noreply.github.com>
2022-05-24 14:53:31 -0700

    Tried Magic

--- Enemy.java
+++ Enemy.java
@@ -41,8 +41,46 @@ public class Enemy {
     }
     public void enemyTakeAttack(int HPtaken, Random battle_hit_limit) {
         int maybe_miss = battle_hit_limit.nextInt(101);
-        if (maybe_miss <= enemyAccuracy) {
+        if (maybe_miss <= enemyEvasion) {
             enemyHP -= HPtaken;
+            System.out.println("The " + name_type + " loses " + HPtaken + " health points! \n");
+        }
+        else {
+            System.out.println("The " + name_type + " dodges! \n");
+        }
+    }
+    
+    //Magic attacks:
+    public void enemyMagicAttack(Hilda hilda, Random battle_hit_limit) {
+        System.out.println("The " + name_type + " attacks!");
+        int maybe_hit = battle_hit_limit.nextInt(101);
+        if (maybe_hit <= enemyAccuracy) {
+            hilda.hildaTakeAttack(enemyStrength/2 + 5, battle_hit_limit);
+        }
+        else {
+            System.out.println("The " + name_type + " misses!");
+            System.out.println("\n");
+        }
+    }
+    public void enemyTakeMagicAttack(String magic_type, int HPtaken, Random battle_hit_limit) {
+        int maybe_miss = battle_hit_limit.nextInt(101);
+        int magic_affinity;
+        //find magic_affinity factor
+        if(magic_type.equals("Fire")) {
+            magic_affinity = enemyFireAff;
+        }
+        else if(magic_type.equals("Water")) {
+            magic_affinity = enemyFireAff;
+        }
+        else if(magic_type.equals("Ice")) {
+            magic_affinity = enemyFireAff;
+        }
+        else {
+            System.out.println("Error.");
+        }
+        //actual damage
+        if (maybe_miss <= enemyEvasion) {
+            enemyHP -= ((HPtaken * magic_affinity)/100);
             System.out.println("The " + name_type + " loses " + HPtaken + " health points! \n");
         }
         else {
